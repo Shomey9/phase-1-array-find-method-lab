48979f1aa61e9faf1aec6fe8d4d5146304f68581
Muhammad Rendusara <hadyanrendusara@yahoo.com>
2021-11-04 11:20:04 -0500

    commit tree 4fe2b7fda459151b2a4feac2f5d54aeb6ccd2ec3

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+]
 
+const superbowlWin = function(array) { 
+    // if (result === "W") {
+
+    // // } else {
+    //     return undefined
+    // }
+   
+    // let yearChiefsWon = array.find(({result}) => {
+    //     return (result === "W")
+    // });
+    // if yearChiefsWon
+    // return yearChiefsWon.year;
+
+    //Keep track in the event that find doesn't 
+    //find anything
+    let yearChiefsWon = array.find(({result}) => {
+        return (result === "W")
+    });
+    console.log(yearChiefsWon); 
+    if (yearChiefsWon === undefined) {
+        return yearChiefsWon;
+    } 
+    else if (yearChiefsWon.result === "W") {
+        return yearChiefsWon.year;  
+    } 
+        
+    }
+    // } elseif (yearChiefsWon.result !== "W") {
+    //     return undefined;
+
+
+
+console.log(superbowlWin(record));
